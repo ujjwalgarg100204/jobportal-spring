8a9a7aa080f10dd7fc5a9170eacd3ef9a475a5c5
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-06-26 19:22:01 +0530

    chore: Create user roles during application startup

--- src/main/java/com/ujjwalgarg/jobportal/JobportalApplication.java
+++ src/main/java/com/ujjwalgarg/jobportal/JobportalApplication.java
@@ -1,8 +1,18 @@
 package com.ujjwalgarg.jobportal;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ujjwalgarg.jobportal.constant.ERole;
+import com.ujjwalgarg.jobportal.entity.Role;
+import com.ujjwalgarg.jobportal.service.RoleService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class JobportalApplication {
 
@@ -10,4 +20,28 @@ public class JobportalApplication {
         SpringApplication.run(JobportalApplication.class, args);
     }
 
+    @Bean
+    @Transactional
+    public CommandLineRunner createUserRoles(RoleService roleService) {
+        return runner -> {
+            Role candidateRole = Role.builder()
+                    .id(1)
+                    .name(ERole.CANDIDATE)
+                    .build();
+            Role recruiterRole = Role.builder()
+                    .id(2)
+                    .name(ERole.RECRUITER)
+                    .build();
+
+            if (!roleService.checkIfExistsByName(candidateRole.getName())) {
+                log.info("Created *Candidate* role in database");
+                roleService.createNew(candidateRole);
+            }
+            if (!roleService.checkIfExistsByName(recruiterRole.getName())) {
+                log.info("Created *Recruiter* role in database");
+                roleService.createNew(recruiterRole);
+            }
+
+        };
+    }
 }
